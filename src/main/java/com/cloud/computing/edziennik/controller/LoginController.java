207c1851f609dac68f503650deed940e057ba191
Kamil Bojarski <kbojarski@sii.pl>
2021-06-08 22:45:59 +0200

    next commit

--- src/main/java/com/cloud/computing/edziennik/controller/LoginController.java
+++ src/main/java/com/cloud/computing/edziennik/controller/LoginController.java
@@ -12,12 +12,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 import static com.cloud.computing.edziennik.entity.Grade.*;
 
-@Controller
+@RestController
 public class LoginController {
 
     private SubjectRepository subjectRepository;
